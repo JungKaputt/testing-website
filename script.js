b67f9cdafe23b09783b469a4f95976b82a0de1e3
JungKaputt <nobelmarchiano53@gmail.com>
2025-03-25 09:59:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -99,3 +99,14 @@ function getWeather() {
             document.getElementById('weather-info').innerText = "Kota tidak ditemukan!";
         });
 }
+
+function login () {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "1234") {
+        window.location.href = "home.html";
+    } else {
+        alert("Username atau password salah!")
+    }
+}
